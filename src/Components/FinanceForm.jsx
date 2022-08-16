68873a528080bf8195e0d2c588f797f079f5f7e1
Harriet Blundell <58891140+Harriet-Blundell@users.noreply.github.com>
2022-08-16 14:12:13 +0100

    Set up finance form component; state and styling

--- src/Components/FinanceForm.jsx
+++ src/Components/FinanceForm.jsx
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import "../Styles/FinanceForm.css";
 
 export default function FinanceForm(aboutYouFormValues) {
+  const [financeFormValues, setFinanceFormValues] = useState({
+    income: "",
+    employment: "",
+  });
+
+  function handleFormChanges(e) {
+    const { name, value } = e.target;
+    setFinanceFormValues((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  }
+
   return (
-    <div>
-      <h1>Finance Form</h1>
+    <div className="finance-form-container">
+      <h1 className="title">Your finances</h1>
+      <div className="income-container">
+        <label className="form-label">Annual income (before tax)</label>
+        <span className="currency-input">
+          £
+          <input
+            type="number"
+            name="income"
+            value={financeFormValues.income}
+            onChange={handleFormChanges}
+            className="income-input"
+            placeholder="Annual income"
+          />
+          per year
+        </span>
+      </div>
+      <div className="employment-container">
+        <label className="form-label">Employment status</label>
+        <select
+          id="employment"
+          className="option-container"
+          name="employment"
+          value={financeFormValues.employment}
+          onChange={handleFormChanges}
+        >
+          <option value="" selected disabled hidden>
+            Select
+          </option>
+          <option value="Full time">Full time</option>
+          <option value="Part time">Part time</option>
+          <option value="Self employed">Self employed</option>
+          <option value="Student">Student</option>
+        </select>
+      </div>
     </div>
   );
 }
