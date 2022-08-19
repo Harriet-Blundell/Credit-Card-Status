a55227a405f1b31bcf84ba8921719ff2236b4fe4
Harriet Blundell <58891140+Harriet-Blundell@users.noreply.github.com>
2022-08-19 13:08:08 +0100

    fix api key error

--- src/Components/PostcodeLookupComponent.jsx
+++ src/Components/PostcodeLookupComponent.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect, createRef } from "react";
 import { PostcodeLookup } from "@ideal-postcodes/postcode-lookup";
-import { config } from "../config.js";
 
 export default function PostcodeLookupComponent(props) {
   const context = createRef();
 
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
   useEffect(() => {
     PostcodeLookup.setup({
-      apiKey: `${config.apiKey}`,
+      apiKey: `${API_KEY}`,
       context: context.current,
       ...props,
     });
-  }, [context, props]);
+  }, [context, props, API_KEY]);
 
   return <div ref={context} className="postcode-lookup-container"></div>;
 }
